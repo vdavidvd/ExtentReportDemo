43ca3efa1ed0f335162d184569a7f58d62c6193c
David <vuksicdavid@gmail.com>
2022-01-25 11:39:00 +0100

    2 commit

--- ExtentDemo/src/main/java/com/basePackage/BaseClass.java
+++ ExtentDemo/src/main/java/com/basePackage/BaseClass.java
@@ -10,10 +10,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import com.extentManager.ExtentManager;
 
